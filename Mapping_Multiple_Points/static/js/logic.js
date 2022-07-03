494dd813d645174fa9d11e472fbfe585ee6116b7
Ricardo Guerreiro <102628808+ricardoguerreiro92@users.noreply.github.com>
2022-07-02 21:34:47 -0400

    Add Multiple Points Marker & Popup Messages on them

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("Working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+let map = L.map("mapid").setView([29.7604, -95.3698], 5);
 
 //// Another method for the above
 // let map2 = L.map("mapid", {
@@ -45,17 +45,34 @@ let darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 darkmap.addTo(map);
 
 // // Add light-yellow circle marker to map with black line
-// L.circle([34.0522, -118.2437], {
-//     radius: 300, //This radius is in meter because it is a L.circle
+L.circle([34.0522, -118.2437], {
+    radius: 300, //This radius is in meter because it is a L.circle
+    fillColor: "#FFFFBF",
+    fillOpacity: 0.2,
+    color: "black"
+}).addTo(map);
+
+// Add light-yellow circle marker to map with black line
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300, //This radius is in pixels because it is a L.circleMarker
 //     fillColor: "#FFFFBF",
 //     fillOpacity: 0.2,
 //     color: "black"
 // }).addTo(map);
 
-// Add light-yellow circle marker to map with black line
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300, //This radius is in pixels because it is a L.circleMarker
-    fillColor: "#FFFFBF",
-    fillOpacity: 0.2,
-    color: "black"
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    // Below is how to add a marker for each city with a popup message
+    // L.marker(city.location).bindPopup("<h2><center>" + city.city + "," + city.state + "</center></h2><hr><h3>Population: " + city.population.toLocaleString() + "</h3>").addTo(map);
+    // Below is how to add a circleMarker for each city with a popup message and radius w/ population size
+    L.circleMarker(city.location,{
+        // skillDrill 13.4.2 - change radius to divide it by 200,000, add color orange and lineweight
+        radius: (city.population - 200000) / 100000,
+        color: "orange",
+        lineWeight: 4
+    }).bindPopup("<h2><center>" + city.city + "," + city.state + "</center></h2><hr><h3>Population: " + city.population.toLocaleString() + "</h3>").addTo(map);
+});
